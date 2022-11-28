04d5c27ac20b6132aaf89e1a681abf5c702d7d3a
drjofu <drjofu@web.de>
2022-11-28 16:28:15 +0100

    Tag 1

---
+++ Ressourcen2/Ressourcen2/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ressourcen2
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+      var brush = (LinearGradientBrush)Resources["LGB"];
+      brush.GradientStops[0].Color = Colors.Red;
+    }
+
+    private void Button_Click_1(object sender, RoutedEventArgs e)
+    {
+      Resources["LGB"] = new LinearGradientBrush(Colors.Orange, Colors.Pink, 30);
+    }
+  }
+}

c37ccd09491cf7c92c0e30ff12272bdd1c7a428b
drjofu <drjofu@web.de>
2022-11-29 10:30:45 +0100

    Tag 2

--- Datenbindung1/Datenbindung1/App.xaml.cs
+++ Datenbindung1/Datenbindung1/App.xaml.cs
@@ -2,16 +2,32 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Globalization;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Markup;
 
 namespace Datenbindung1
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+  /// <summary>
+  /// Interaction logic for App.xaml
+  /// </summary>
+  public partial class App : Application
+  {
+    static App()
     {
+      Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("en-US");
+      Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo("en-US");
+      FrameworkElement.LanguageProperty.OverrideMetadata(
+        typeof(FrameworkElement),
+        new FrameworkPropertyMetadata(
+          XmlLanguage.GetLanguage(
+            CultureInfo.CurrentCulture.Name)
+          )
+        );
+
     }
+  }
 }
---
+++ Datenbindung2/Datenbindung2/Person.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace Datenbindung2
+{
+  class Person
+  {
+    public string Name { get; set; }
+    public string Wohnort { get; set; }
+    public int Alter { get; set; }
+
+    public bool IstErfahren => Alter > 50;
+  }
+
+  class Firma
+  {
+    public string Name { get; set; } = "Hintz & Kuntz";
+    public List<Person> Mitarbeiter { get; set; } = new List<Person>()
+    {
+      new (){Name="Petra", Wohnort="Köln", Alter=44},
+      new (){Name="Marie", Wohnort="Ulm", Alter=37 },
+      new (){Name="Karin", Wohnort="Hamburg", Alter=44},
+      new (){Name="Uwe", Wohnort="Köln", Alter=54},
+      new (){Name="Klaus", Wohnort="München",Alter=66}
+    };
+  }
+}

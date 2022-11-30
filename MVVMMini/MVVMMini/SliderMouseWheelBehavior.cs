c63a3cf548b70c5eba1b3857aaa18e99606ea11b
drjofu <drjofu@web.de>
2022-11-30 13:52:43 +0100

    Tag 3

---
+++ MVVMMini/MVVMMini/SliderMouseWheelBehavior.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace MVVMMini
+{
+  internal class SliderMouseWheelBehavior : Behavior<Slider>
+  {
+    protected override void OnAttached()
+    {
+      base.OnAttached();
+      this.AssociatedObject.MouseWheel += AssociatedObject_MouseWheel;
+    }
+
+    protected override void OnDetaching()
+    {
+      this.AssociatedObject.MouseWheel -= AssociatedObject_MouseWheel;
+      base.OnDetaching();
+    }
+
+    private void AssociatedObject_MouseWheel(object sender, System.Windows.Input.MouseWheelEventArgs e)
+    {
+      this.AssociatedObject.Value += e.Delta * this.AssociatedObject.SmallChange;
+    }
+  }
+}

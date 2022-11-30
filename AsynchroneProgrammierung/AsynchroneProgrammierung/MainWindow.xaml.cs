d2376e6d94d1c2402f8782dab23397008eab62c1
drjofu <drjofu@web.de>
2022-11-30 16:29:29 +0100

    Tag 3

---
+++ AsynchroneProgrammierung/AsynchroneProgrammierung/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace AsynchroneProgrammierung
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    private DispatcherTimer timer;
+    private long zähler;
+    private object syncObj = new object();
+
+    public MainWindow()
+    {
+      InitializeComponent();
+      timer = new DispatcherTimer();
+      timer.Interval = TimeSpan.FromMilliseconds(100);
+      timer.Tick += Timer_Tick;
+      timer.Start();
+    }
+
+    private void Timer_Tick(object? sender, EventArgs e)
+    {
+      LBL.Content = zähler;
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+      //Inkrementieren();
+      Task.Run(Inkrementieren);
+      //Task.Factory.StartNew(,,TaskCreationOptions.LongRunning)
+    }
+
+    private void Inkrementieren()
+    {
+      for (long i = 0; i < 100_000_000; i++)
+      {
+        //Monitor.Enter(syncObj);
+
+        lock (syncObj)
+        {
+          zähler++;
+        }
+
+        //Monitor.Exit(syncObj);
+      }
+    }
+  }
+}
---
+++ AsynchroneProgrammierung/AsynchroneProgrammierung/Window1.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AsynchroneProgrammierung
+{
+  /// <summary>
+  /// Interaction logic for Window1.xaml
+  /// </summary>
+  public partial class Window1 : Window
+  {
+    private CancellationTokenSource cts;
+    private CancellationToken ct;
+
+    public Window1()
+    {
+      InitializeComponent();
+    }
+
+    private async void BTN_Start_Click(object sender, RoutedEventArgs e)
+    {
+      cts = new CancellationTokenSource();
+      ct = cts.Token;
+
+      BTN_Start.IsEnabled = false;
+      //Task
+      //  .Run(Inkrementieren)
+      //  .ContinueWith(t => BTN_Start.IsEnabled = true,TaskScheduler.FromCurrentSynchronizationContext());
+
+      Task t = Task.Run(Inkrementieren, ct);
+
+      try
+      {
+        await t;
+      }
+      catch (Exception ex)
+      {
+      }
+
+      //await Task.WhenAll(t);
+      BTN_Start.IsEnabled = true;
+      //await Task.Run(...)
+      try
+      {
+        var n = await Zwischenschritt();
+        LBL.Content = n;
+      }
+      catch (Exception)
+      {
+      }
+    }
+
+    private Task<int> Zwischenschritt()
+    {
+      return AntwortAufDieFrageAllerFragen();
+    }
+
+    private async Task<int> AntwortAufDieFrageAllerFragen()
+    {
+      await Task.Delay(5000, ct);
+      return 42;
+    }
+
+    private void Inkrementieren()
+    {
+      int i;
+      for (i = 0; i < 5; i++)
+      {
+        if (ct.IsCancellationRequested)
+        {
+          // aufräumen...
+          ct.ThrowIfCancellationRequested();
+        }
+
+        int k = i;
+        Thread.Sleep(1000);
+        //LBL.Content = i;
+        //Dispatcher.BeginInvoke(() => LBL.Content = i);
+        Dispatcher.BeginInvoke(() => LBL.Content = k);
+        //Dispatcher.BeginInvoke(() => Debug.WriteLine(k));  // Closure
+      }
+    }
+
+    private void BTN_Stopp_Click(object sender, RoutedEventArgs e)
+    {
+      cts.Cancel();
+    }
+  }
+}
